a4ca1e9172e6f08a4bcb48d40d68c4f72c61c036
Ginkgo <leisun1101@gmail.com>
2023-11-07 23:22:57 +0100

    Create prepare_yum.sh

---
+++ prepare_yum.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+sudo yum update 
+sudo yum install zsh autojump -y
+
+sudo chsh -s $(which zsh) $USER
+
+zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+sudo yum install tmux -y
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+curl -sLf https://spacevim.org/install.sh | bash
+
+curl -sS https://starship.rs/install.sh | sh
+
+rm ~/.zshrc
+wget https://raw.githubusercontent.com/GinkgoCoder/machine-setup/main/.zshrc
