b7e20f5be4bd314c79eb01119ad2de6844929da7
Ginkgo <leisun1101@gmail.com>
2023-11-07 23:53:43 +0100

    Update prepare_yum.sh

--- prepare_yum.sh
+++ prepare_yum.sh
@@ -5,7 +5,7 @@ sudo yum install zsh autojump -y
 
 sudo chsh -s $(which zsh) $USER
 
-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
